b0ef3515770d721c764a274b73d186fe5b07444d
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-09-27 09:35:58 +0200

    Create new post added

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from datetime import datetime
 import sqlite3
 from werkzeug.exceptions import abort
@@ -22,6 +22,10 @@ def get_post(post_id):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '771006'
+
+
+
 
 @app.route("/")
 def index():
@@ -37,6 +41,23 @@ def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
 
 
 
