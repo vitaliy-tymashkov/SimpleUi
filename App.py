482f10c08fdc9f239187d09d2c61cdd330196c2d
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-09-27 09:17:39 +0200

    index.html updated with db content

--- App.py
+++ App.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 from datetime import datetime
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == "__main__":
     dateTimeObj = datetime.now()
